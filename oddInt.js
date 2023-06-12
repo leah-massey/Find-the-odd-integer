e95be44e2aa35a3a13b5f93f9e453525dbc561f6
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-12 10:52:41 +0100

    Test 3 passing: an array with multiple values returns correct answer.

--- oddInt.js
+++ oddInt.js
@@ -1,5 +1,17 @@
-const oddInt = function (array) {
-  return Number(array.join(""));
+const oddInt = function (numberArray) {
+  let oddNumber;
+  numberArray.forEach((number) => {
+    let numberCount = 0;
+    numberArray.forEach((newNumber) => {
+      if (newNumber === number) {
+        numberCount++;
+      }
+    });
+    if (numberCount % 2 !== 0) {
+      oddNumber = number;
+    }
+  });
+  return oddNumber;
 };
 
 module.exports = oddInt;
--- oddInt.test.js
+++ oddInt.test.js
@@ -8,4 +8,8 @@ describe("oddInt", () => {
   it("returns the value that occurs an odd number of times", () => {
     expect(oddInt([0])).toEqual(0);
   });
+
+  it("returns the value that occurs an odd number of times when multiple values are given", () => {
+    expect(oddInt([1, 1, 2])).toEqual(2);
+  });
 });
